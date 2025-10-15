276e12f6f416efcf963dcc80d2bfe938a04fdcae
Emely <emelysantina00@gmail.com>
2025-10-15 17:47:14 -0300

    initial commit

---
+++ src/test/java/com/BiblioStock/BiblioStock_API/BiblioStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BiblioStock.BiblioStock_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
